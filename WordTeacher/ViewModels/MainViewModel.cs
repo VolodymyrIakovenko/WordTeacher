33154ae9e793704307bae790fa983ca7c1e9ec75
vayak38 <vayak38@mail.ru>
2015-06-12 23:32:01 +0200

    Added context menu and settings empty window.

--- WordTeacher/ViewModels/MainViewModel.cs
+++ WordTeacher/ViewModels/MainViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
 using System.ComponentModel;
-using WordTeacher.Models;
+using System.Windows;
+using System.Windows.Input;
+
+using WordTeacher.Commands;
 using WordTeacher.Utilities;
+using WordTeacher.Views;
 
 namespace WordTeacher.ViewModels
 {
@@ -9,7 +13,16 @@ namespace WordTeacher.ViewModels
     {
         private double _positionX;
         private double _positionY;
-        private ObservableCollection<ContextAction> _contextActions = new ObservableCollection<ContextAction>();
+        private bool _isSettingsOpened;
+        private ICommand _closeCommand;
+        private ICommand _settingsCommand;
+
+        public MainViewModel()
+        {
+            ArrangeWindowPosition();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
         /// Position of the window on the X axis.
@@ -38,22 +51,39 @@ namespace WordTeacher.ViewModels
         }
 
         /// <summary>
-        /// List of the context menu items.
+        /// Position of the window on the Y axis.
         /// </summary>
-        public ObservableCollection<ContextAction> ContextActions
+        public bool IsSettingsOpened
         {
-            get { return _contextActions; }
+            get { return _isSettingsOpened; }
             set
             {
-                _contextActions = value;
-                OnPropertyChanged("ContextActions");
+                _isSettingsOpened = value;
+                OnPropertyChanged("IsSettingsOpened");
             }
         }
 
-        public MainViewModel()
+        public ICommand SettingsCommand
         {
-            ArrangeWindowPosition();
-            CreateContextItems();
+            get
+            {
+                return _settingsCommand ?? (_settingsCommand = new CommandHandler(OpenSettings, true));
+            }
+        }
+
+        public ICommand CloseCommand
+        {
+            get
+            {
+                return _closeCommand ?? (_closeCommand = new CommandHandler(CloseApplication, true));
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
         }
 
         /// <summary>
@@ -66,27 +96,23 @@ namespace WordTeacher.ViewModels
             PositionY = topCenterPoint.Y;
         }
 
-        /// <summary>
-        /// Add context menu items.
-        /// </summary>
-        private void CreateContextItems()
+        private void OpenSettings()
         {
-            ContextActions = new ObservableCollection<ContextAction>
-            {
-                new ContextAction("Settings", null),
-                new ContextAction("Exit", null)
-            };
+            var settingsView = new SettingsView();
+            settingsView.Closed += SettingsViewOnClosed;
+            settingsView.Show();
 
-            OnPropertyChanged("ContextActions");
+            IsSettingsOpened = true;
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
+        private void SettingsViewOnClosed(object sender, EventArgs eventArgs)
         {
-            var handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(name));
+            IsSettingsOpened = false;
+        }
+
+        private static void CloseApplication()
+        {
+            Application.Current.Shutdown();
         }
     }
 }
